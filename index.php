ee6910b6c832579929296d362bc19201edf30d57
sandesh <sandeshthakur881@gmail.com>
2022-08-24 20:48:03 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.rtl.min.css">
+
+    <title>php</title>
+  </head>
+  <body>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+   
+  </body>
+</html>
---
+++ student-create.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.rtl.min.css">
+
+    <title>php</title>
+  </head>
+  <body>
+
+
+  <div class="container ">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4> Student Add
+                        <a href="index.php" class="btn btn-primary float-md-start">Back</a>
+                </div>
+                <div class="card-body">
+                    <form action="" method="POST">
+
+                    <div class="mb-3">
+                        <label>Student Name</label>
+                        <input type="text" names="Name" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label>Student Email</label>
+                        <input type="text" names="Email" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label>Student Phone</label>
+                        <input type="text" names="Phone" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label>Student Course</label>
+                        <input type="text" names="Course" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <button type="Submit" class= "btn btn-primary">Save Student</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+   
+  </body>
+</html>
